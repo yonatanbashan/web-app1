36af028ed14f858a6787b6d28e32eda397527e46
yonatanbashan <yonatan.bashan@gmail.com>
2018-11-21 17:52:52 +0200

    For prod

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -1,11 +1,11 @@
+import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
-
 
 @Injectable()
 export class ConnectionService {
 
   getServerAddress() {
-    return 'http://localhost:3000/';
+    return environment.apiUrl;
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://bloggism2-env.w5xmp2hbj7.us-east-2.elasticbeanstalk.com/"
 };
