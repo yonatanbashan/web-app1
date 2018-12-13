d09e5541d4caabafcb44144a4e992e5b647c60f6
yonatanbashan <yonatan.bashan@gmail.com>
2018-12-13 16:31:27 +0200

    styling - more

--- backend/common/app-config.js
+++
@@ -1,9 +0,0 @@
-const appConfig = {
-  AWSAddress: "http://bloggism-yonatan-bashan.s3-website.us-east-2.amazonaws.com",
-  imagesFolder: "user-images",
-  AWSAccessKeyId: 'AKIAJTXYQQZX4WOE2B7Q',
-  AWSSecretKey: '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',
-  AWSBucketName: 'bloggism-yonatan-bashan',
-}
-
-module.exports = appConfig;
